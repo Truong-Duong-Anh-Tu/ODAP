93c9e3cb4709c7e174f643d3077f017ecee0cc0a
Truong-Duong-Anh-Tu <truongtu11747@gmail.com>
2025-06-19 22:40:09 +0700

    Fix to run code

--- spark_streaming_consumer.py
+++ spark_streaming_consumer.py
@@ -14,7 +14,7 @@ from exchange_rate import current_exchange_rate, fetch_exchange_rate, update_exc
 spark = SparkSession.builder \
     .appName("KafkaCreditCardStreaming") \
     .master("local[*]") \
-    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.4") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.5") \
     .getOrCreate()
 
 spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
@@ -68,45 +68,68 @@ invalid_df2 = invalid_df1.filter(col("Is Fraud?") == "No")
 
 # Định nghĩa hàm xử lý cho mỗi batch
 def process_batch(batch_df, batch_id):
-    if batch_df.count() > 0:
-        # Lấy tỷ giá hiện tại cho batch này
-        current_rate = current_exchange_rate
-        print(f"Batch {batch_id}: Sử dụng tỷ giá {current_rate}")
-        
-        # Xử lý batch với tỷ giá hiện tại
-        processed_batch = batch_df \
-            .withColumn("Time_fixed", when(length(col("Time")) <= 5, concat_ws(":", col("Time"), lit("00"))).otherwise(col("Time"))) \
-            .withColumn("datetime_str", concat_ws(" ", concat_ws("/", col("Day"), col("Month"), col("Year")), col("Time_fixed"))) \
-            .withColumn("timestamp", to_timestamp(col("datetime_str"), "dd/MM/yyyy HH:mm:ss")) \
-            .withColumn("date", date_format(col("timestamp"), "dd/MM/yyyy")) \
-            .withColumn("time", date_format(col("timestamp"), "HH:mm:ss")) \
-            .withColumn("Amount_Cleaned", regexp_replace(trim(col("Amount")), "[$,]", "")) \
-            .withColumn("Current_Exchange_Rate", lit(current_rate)) \
-            .withColumn("Amount_VND", col("Amount_Cleaned").cast("double") * col("Current_Exchange_Rate"))
-        
-        # Ghi batch vào HDFS
-        processed_batch.select(
-            "Card", "date", "time", "Merchant Name", "Merchant City", "Amount_VND"
-        ).write \
-            .mode("append") \
-            .option("header", "true") \
-            .format("csv") \
-            .save("hdfs://localhost:9000/processed_data/transactions")
-        
-        # Tính toán và ghi thống kê
-        agg_batch = processed_batch \
-            .withWatermark("timestamp", "1 day") \
-            .groupBy(window(col("timestamp"), "1 day"), col("Merchant Name")) \
-            .agg(
-                count("*").alias("num_transactions"),
-                sum("Amount_VND").alias("total_amount_vnd")
-            )
-        
-        agg_batch.write \
-            .mode("append") \
-            .option("header", "true") \
-            .format("csv") \
-            .save("hdfs://localhost:9000/processed_data/statistics")
+
+    try:
+        if batch_df.head(1):
+            # Lấy tỷ giá hiện tại cho batch này
+            current_rate = current_exchange_rate
+            print(f"Batch {batch_id}: Sử dụng tỷ giá {current_rate}")
+            
+            # Xử lý batch với tỷ giá hiện tại
+            processed_batch = batch_df \
+                .withColumn("Time_fixed", when(length(col("Time")) <= 5, concat_ws(":", col("Time"), lit("00"))).otherwise(col("Time"))) \
+                .withColumn("datetime_str", concat_ws(" ", concat_ws("/", col("Day"), col("Month"), col("Year")), col("Time_fixed"))) \
+                .withColumn("timestamp", to_timestamp(col("datetime_str"), "dd/MM/yyyy HH:mm:ss")) \
+                .withColumn("date", date_format(col("timestamp"), "dd/MM/yyyy")) \
+                .withColumn("time", date_format(col("timestamp"), "HH:mm:ss")) \
+                .withColumn("Amount_Cleaned", regexp_replace(trim(col("Amount")), "[$,]", "")) \
+                .withColumn("Current_Exchange_Rate", lit(current_rate)) \
+                .withColumn("Amount_VND", col("Amount_Cleaned").cast("double") * col("Current_Exchange_Rate"))
+            
+            # Ghi batch vào HDFS
+            processed_batch.select(
+                "Card", "date", "time", "Merchant Name", "Merchant City", "Amount_VND"
+            ).write \
+                .mode("append") \
+                .option("header", "true") \
+                .format("csv") \
+                .save("hdfs://localhost:9000/processed_data/transactions")
+            
+            # Tính toán và ghi thống kê
+            # agg_batch = processed_batch \
+            #     .withWatermark("timestamp", "1 day") \
+            #     .groupBy(window(col("timestamp"), "1 day"), col("Merchant Name")) \
+            #     .agg(
+            #         count("*").alias("num_transactions"),
+            #         sum("Amount_VND").alias("total_amount_vnd")
+            #     )
+            
+            agg_batch = processed_batch \
+                .withWatermark("timestamp", "1 day") \
+                .groupBy(window(col("timestamp"), "1 day"), col("Merchant Name")) \
+                .agg(
+                    count("*").alias("num_transactions"),
+                    sum("Amount_VND").alias("total_amount_vnd")
+                ) \
+                .withColumn(
+                    "window_str",
+                    concat_ws(" - ",
+                        date_format(col("window.start"), "yyyy-MM-dd"),
+                        date_format(col("window.end"), "yyyy-MM-dd")
+                    )
+                ) \
+                .drop("window")
+            
+            agg_batch.write \
+                .mode("append") \
+                .option("header", "true") \
+                .format("csv") \
+                .save("hdfs://localhost:9000/processed_data/statistics")
+            
+    except Exception:
+        import traceback
+        traceback.print_exc()
+        raise
 
 # Áp dụng function xử lý batch
 query = invalid_df2.writeStream \
